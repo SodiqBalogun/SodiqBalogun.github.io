8100ecc6c63ba1d65665781b404597b163f93f22
SodiqBalogun <sbalogun157@gmail.com>
2024-07-25 15:57:16 -0500

    Implemented right scrolling functionality on the coffee section of menu.html. This will be a very robust undertaking to implement for all sections.

--- Project_Files/script.js
+++ Project_Files/script.js
@@ -53,3 +53,42 @@ function scrollToElement(pageElement) {
         behavior: "smooth"
     });
 }
+
+// Scrolling Coffee
+const coffeeArr = ["Latte", "Cappuccino", "Cortado", "Mocha", "Drip Coffee", "Red Eye", "Cafe Au Lait", "Americano"];
+let current = document.getElementById("content");
+let currentIndex = 0;
+let leftIndex = coffeeArr.length - 1;
+let rightIndex = 1;
+let leftButton = document.getElementById("slideLeft");
+let rightButton = document.getElementById("slideRight");
+current.innerHTML = coffeeArr[currentIndex];
+leftButton.innerHTML = coffeeArr[leftIndex];
+rightButton.innerHTML = coffeeArr[rightIndex];
+
+function scrollCoffeesRight() {
+    if (currentIndex !== coffeeArr.length - 1) {
+        currentIndex++;
+        if (leftIndex === coffeeArr.length - 1) {
+            leftIndex = 0;
+        } else {
+            leftIndex++;
+        }
+        if (rightIndex === coffeeArr.length - 1) {
+            rightIndex = 0;
+        } else {
+            rightIndex++;
+        }
+        current.innerHTML = coffeeArr[currentIndex];
+        leftButton.innerHTML = coffeeArr[leftIndex];
+        rightButton.innerHTML = coffeeArr[rightIndex];
+    }
+    if (currentIndex === coffeeArr.length - 1) {
+        current.innerHTML = coffeeArr[currentIndex];
+        leftButton.innerHTML = coffeeArr[leftIndex];
+        rightButton.innerHTML = coffeeArr[rightIndex];
+        currentIndex = 0;
+        leftIndex = coffeeArr.length - 1;
+        rightIndex = 1;
+    }
+}
